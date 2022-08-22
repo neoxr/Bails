6def7274e4767c8a312ca3e1391a7351bac43718
neoxr <neoxr@null.net>
2022-08-22 18:24:52 +0700

    upgrade

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -748,7 +748,7 @@ const generateContextInfo = () => {
 export const patchMessageForMdIfRequired = (message: proto.IMessage) => {
 	const requiresPatch = !!(
 		message.buttonsMessage
-		// || message.templateMessage
+		|| message.templateMessage
 		|| message.listMessage
 	)
 	if(requiresPatch) {
